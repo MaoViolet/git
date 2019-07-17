5d09818de586bd77f9887d292cb97b956e4ecdca
MaoViolet <2979657955qq.com>
2019-07-17 16:19:15 +0800

    第二个

--- helloworld01/src/com/helloworld/study/Mao.java
+++ helloworld01/src/com/helloworld/study/Mao.java
@@ -1,11 +1,31 @@
 package com.helloworld.study;
 
+import java.util.Scanner;
+
 public class Mao {
 	
+	private static Scanner in;
+
 	public static void main(String[] args) {
-	
+		setIn(new Scanner(System.in));
+		int foot;
+		int inch;
+		foot=in.nextInt();
+		inch=in.nextInt();
+		System.out.println((foot+inch/12.0)*0.3048);
 		
-		System.out.println("heiio.world");
+		
+		
+		
+//		System.out.println("heiio.world");
+	}
+
+	public static Scanner getIn() {
+		return in;
+	}
+
+	public static void setIn(Scanner in) {
+		Mao.in = in;
 	}
 
 }
