ffd781179755dabd533ee24b470c756d3ab8cbc7
MaoViolet <2979657955qq.com>
2019-07-24 16:19:33 +0800

    第一次尝试while循环

---
+++ helloworld01/src/com/helloworld/study/Scanner.java
@@ -0,0 +1,28 @@
+package com.helloworld.study;
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A =0;
+		int S=0;
+//			
+		Scanner in = new Scanner();
+		while(true)
+		{
+		if(A <=100)
+		{
+			A=A+1;
+			S=S+A;
+		}
+		
+		
+		System.out.println("S="+S);
+	}
+}
+}
+	
+	
+
+
+
