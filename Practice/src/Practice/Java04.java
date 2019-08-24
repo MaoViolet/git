ab1fdf8716cc0fa13c47b6c460cbccfddac9d85d
MaoViolet <2979657955qq.com>
2019-08-24 11:26:47 +0800

    2019.08.24

--- Practice/src/Practice/Java04.java
+++ Practice/src/Practice/Java04.java
@@ -1,4 +1,9 @@
 package Practice;
+/**
+ * 遍历数组
+ * 编写人：毛程浩
+ * 2019.08.24
+ */
 
 import java.util.Scanner;
 
@@ -7,12 +12,36 @@ public class Java04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in =new Scanner(System.in);
+		int[] data = {2,3,4,5,7,9,6,23,54};
+		int x = in.nextInt();
+		int loc = -1;
+		for(int i=0; i<data.length;i++)
+		{
+			if(x==data[i])
+			{
+				loc = i;
+				break;
+			}
+		}
+		if(loc<-1)
+		{
+			System.out.println(x+"是第"+(loc+1)+"个");
+		}
+		else
+		{
+			System.out.println("不在其中");
+		}
+				
 		
-		System.out.println("请输入票面:");
-		int amount = in.nextInt();
-		System.out.println("请输入金额:");
-		int price = in.nextInt();
-		System.out.println(amount+"-"+price+"="+(amount-price));
+		
+		
+//		
+//		System.out.println("请输入票面:");
+//		int amount = in.nextInt();
+//		System.out.println("请输入金额:");
+//		int price = in.nextInt();
+//		System.out.println(amount+"-"+price+"="+(amount-price));
+		
 
 	}
 
